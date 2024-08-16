25254a5d66b7b4ebe3e27e5e58ef1de14056d435
Viala250 <vialarobinson@gmail.com>
2024-08-16 13:11:55 -0500

    this is my first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+echo "# viala.project" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Viala250/viala.project.git
+git push -u origin main
